eb7c23f7fd2822757c3267172e8435b82a356774
Alinajafi10 <142527995+Alinajafi10@users.noreply.github.com>
2023-09-20 19:12:58 +0330

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
+// for toggler
 const toggler = document.querySelector(".nav__toggler");
 const navbar = document.querySelector(".nav");
 toggler.addEventListener("click", (e) => {
   navbar.classList.toggle("nav__expanded");
+});
+
+// for like icon
+const icon = document.querySelector(".catalog__icon");
+const likebtn = document.querySelector(".like-icon");
+icon.addEventListener("click", (e) => {
+  likebtn.classList.toggle("liked");
 });
